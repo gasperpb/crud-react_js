23fda6f63dd901365e87fd33faddcedfc0865e0d
gasperpb <achilespb@hotmail.com>
2023-10-21 11:40:47 -0300

    em teste

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [data, setData] = useState([]);
+  const [nome, setName] = useState('');
+  const [email, setEmail] = useState('');
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = () => {
+    axios.get('http://localhost:3001/read')
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  const handleCreate = () => {
+    axios.post('http://localhost:3001/create', { nome, email })
+      .then(() => {
+        fetchData();
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  const handleUpdate = (id) => {
+    axios.put(`http://localhost:3001/update/${id}`, { nome, email })
+      .then(() => {
+        fetchData();
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  const handleDelete = (id) => {
+    axios.delete(`http://localhost:3001/delete/${id}`)
+      .then(() => {
+        fetchData();
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>CRUD Example</h1>
+      <div>
+        <input type="text" placeholder="Name" value={nome} onChange={(e) => setName(e.target.value)} />
+        <input type="text" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        <button onClick={handleCreate}>Create</button>
+      </div>
+      <ul>
+        {data.map((user) => (
+          <li key={user.id}>
+            {user.nome} - {user.email}
+            <button onClick={() => handleUpdate(user.id)}>Update</button>
+            <button onClick={() => handleDelete(user.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
